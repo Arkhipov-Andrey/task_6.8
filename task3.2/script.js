ff768bcfb491ab1cdaef638a5d97fd142be6e4fa
Arkhipov-Andrey <Andy-Ar@yandex.ru>
2023-01-14 16:17:14 +0300

    Module 6 Task 6.8 (HW-03) Add comment

--- task3.2/script.js
+++ task3.2/script.js
@@ -1,23 +1,38 @@
+// Светофор дожен отображать сигналы в следующем порядке: красный -> желтый -> зеленый
+// В задании како-то не правильный светофор
+
+// Выбираем нужные элементы
 const red = document.querySelector('#red');
 const yellow = document.querySelector('#yellow');
 const green = document.querySelector('#green');
 
+// Объявляем глобальную переменную для хранения текужего цвета
 var curentColor;
 
+// Привязываем обработчик на событие click
 red.addEventListener('click', trafficLights)
 yellow.addEventListener('click', trafficLights)
 green.addEventListener('click', trafficLights)
 
+// Сделаем универсальную функцию для смены цветов
 function makeColor(element, color = 'black') {
     element.style.background = color;
 }
 
+// Основная функция для работы светофора
+// Если текущий цвет не установлен, то похоже на то, что мы только открыли страницу и все элементы черного цвета
+// Установим текущий цвет в красный и покрасим первый элемент
 function trafficLights() {
     if (!curentColor) {
         curentColor = 'red';
         makeColor(red, curentColor);
     } else {
-
+        // Начинаем поиск текущего цвета
+        // Если какой-то цвет задан
+        // Находим цвет и перекрашиваем соответсвующий цветной элемент в черный
+        // Устанавливаем следущий по порядку цвет
+        // Перекрашиваем следущий элемент в заданный цвет
+        // Повторяем пока не надоест
         if (curentColor === 'red') {
             makeColor(red);
             curentColor = 'yellow';
